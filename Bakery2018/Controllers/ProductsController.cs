a68aaa98bf77bbb6690cf1cea92a2019bea49c72
Bennett <Bkielbon@gmail.com>
2018-03-23 16:53:46 -0700

    Add project files.

---
+++ Bakery2018/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery2018.Models;
+
+namespace Bakery2018.Controllers
+{
+    public class ProductsController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            BakeryEntities db = new BakeryEntities();
+            return View(db.Products.ToList());
+        }
+        public ActionResult Register()
+        {
+            BakeryEntities db = new BakeryEntities();
+            return View(db.Products.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "ProductName, ProductPrice, ProductKey")]Product pr)
+        {
+            db.Products.Add(pr);
+            db.SaveChanges();
+
+            Message msg = new Message("Thank you for registering!");
+            return View("Result", msg);
+        }
+        public ActionResult Result(Message msg)
+        {
+            return View(msg);
+        }
+    }
+}
---
+++ Bakery2018/Controllers/RegistrationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery2018.Models;
+
+namespace Bakery2018.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+
+        }
+        public ActionResult PeopleList()
+        {
+            return View(db.People.ToList());
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "PersonFirstName, PersonLastName, PersonEmail,PersonPhone,Personkey")]Person p)
+        {
+            p.PersonDateAdded = DateTime.Now;
+            db.People.Add(p);
+            db.SaveChanges();
+
+            Message msg = new Message("Thank you for registering!");
+            return View("Result", msg);
+        }
+        public ActionResult Result(Message msg)
+        {
+            return View(msg);
+        }
+    }
+}
