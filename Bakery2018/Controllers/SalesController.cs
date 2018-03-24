05e4ed76d5136513ac8b1cd19fef8fbbdfa6dcff
Bennett <Bkielbon@gmail.com>
2018-03-23 19:17:42 -0700

    Added sales and receipt

---
+++ Bakery2018/Controllers/SalesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery2018.Models;
+
+namespace Bakery2018.Controllers
+{
+    public class SalesController : Controller
+    {
+        BakeryEntities db = new BakeryEntities(); 
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "EmployeeKey, SaleDate, ProductKey, SaleDetailPriceCharged,SaleDetailDiscount,SaleKey")]PointOfSale pos)
+        {
+            pos.SaleDate = DateTime.Now;
+            db.Sales.Add(pos);
+            db.SaveChanges();
+
+            Message msg = new Message("Thank you for registering! Total price is" PointOfSale.SaleDetailPriceCharged);
+            return View("Result", msg);
+        }
+        public ActionResult Result(Message msg)
+        {
+            return View(msg);
+        }
+    }
+}
---
+++ Bakery2018/Models/PointOfSale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bakery2018.Models;
+
+namespace Bakery2018.Models
+{
+    public class PointOfSale
+    {
+        public IEnumerable<Models.Sale> Sale { get; set; }
+        public IEnumerable<Models.SaleDetail>SaleDetails{ get;set;} 
+        public string CustomerKey { get; set; }
+        public string EmployeeKey { get; set; }
+        public string SaleDate { get; set; }
+        public string ProductKey { get; set; }
+        public string SaleKey { get; set; }
+        public string SaleDetailPriceCharged { get; set; }
+        public string SaleDetailDiscount{ get; set; }
+
+
+
+
+
+    }
+}
